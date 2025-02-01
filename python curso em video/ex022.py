21d6527826016326bc107cfd3eb45bdbed40226f
guisaul <guilhermesaul190@gmail.com>
2025-02-01 11:43:34 -0300

    Ler nome completo, exibir em maiúsculas, minúsculas, contar letras (somente letras) e mostrar o tamanho do primeiro nome.

--- python curso em video/ex022.py
+++ python curso em video/ex022.py
@@ -1,6 +1,7 @@
-nome = input('Qual seu nome? ')
-nome1 = nome.upper()
-nome2 = nome.lower()
-nome3 = nome.split()
-dividido = nome3.count[:]
-print(nome1, nome2, nome3)
+nome = str(input("Digite o seu nome completo: ")).strip()
+#nome1 = nome.split()
+print(f"Seu nome completo em maiúsculas: {nome.upper()}")
+print(f"Seu nome completo em minúsculas: {nome.lower()}")
+print(f"Seu nome completo tem: {len(nome) - nome.count(" ")} letras")
+print(f"Seu primeiro nome tem: {nome.find(" ")} letras") 
+#print(f"Seu primeiro nome tem: {len(nome1[0])} letras") mesma coisa da linha 6
